021c11a98258c2bfaeecd00bdb6cfeae290e51dd
Leana Lopez <chileana_go@yahoo.com>
2021-07-01 14:42:55 -0700

    refactored for Render()

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const submitHandler = (event) => {
 
   const form = document.querySelector("#park-form");
   const formData = new FormData(form);
+
   // Keep track of if any errors are found
   let hasErrors = false;
 
@@ -18,6 +19,21 @@ const submitHandler = (event) => {
 
   // if there are no errors
   if (!hasErrors) {
+    // Create an empty object - RENDER REFACTOR
+    const park = {
+      name: formData.get("name"),
+      location: formData.get("location"),
+      description: formData.get("description"),
+      established: formData.get("established"),
+      area: formData.get("area"),
+      rating: formData.get("rating"),
+    };
+
+    parks.push(park);
+
+    render();
+
+    /* BERORE RENDER REFACTOR
     //create a new element
     const parkSection = document.createElement("section");
 
@@ -50,17 +66,36 @@ const submitHandler = (event) => {
     parkSection.innerHTML = content;
 
     //append to the main element
-    document.querySelector("main").appendChild(parkSection);
+    document.querySelector("main").appendChild(parkSection); */
   }
 };
 
 // function to handler favorite button clicks
 const favoriteButtonClickHandler = (event) => {
+  if (event.target && event.target.nodeName == "BUTTON") {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  }
+
+  /* BEFORE RENDER REFACTOR
   const park = event.target.parentNode;
-  park.style.backgroundColor = "#c8e6c9";
+  park.style.backgroundColor = "#c8e6c9"; */
 };
 
-// function for sorting by name
+// function for sorting by name - RENDER REFACTORED
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.name;
+  const parkBName = parkB.name;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+/* before refactor
 const sortByName = (parkA, parkB) => {
   const parkAName = parkA.querySelector("h2").innerText;
   const parkBName = parkB.querySelector("h2").innerText;
@@ -71,45 +106,34 @@ const sortByName = (parkA, parkB) => {
   } else {
     return 0;
   }
-};
+}; */
 
 // function for sorting by rating
 const sortByRating = (parkA, parkB) => {
-  const parkARating = parseFloat(
-    parkA.querySelector(".rating-display > .value").innerText
-  );
-  const parkBRating = parseFloat(
-    parkB.querySelector(".rating-display > .value").innerText
-  );
+  const parkARating = parseFloat(parkA.rating);
+  const parkBRating = parseFloat(parkB.rating);
   return parkBRating - parkARating;
 };
 
-// function for handling the nameSorter click
+// function for handling the nameSorter click - RENDER REFACTORED
 const nameSorterClickHandler = (event) => {
   event.preventDefault();
 
-  // 1.  get the main element
-  const main = document.querySelector("main");
+  parks.sort(sortByName);
 
-  // 2. get the list of parks
-  const parksList = main.querySelectorAll(".park-display");
-
-  // 3. empty the main
-  main.innerHTML = "";
-
-  // 4. create an array
-  const parksArray = Array.from(parksList);
-
-  // 5. sort the array
-  parksArray.sort(sortByName);
-
-  // 6. Insert each park into the DOM
-  parksArray.forEach((park) => {
-    main.appendChild(park);
-  });
+  render();
 };
 
 // function to handle the ratingSorter click
+const ratingSorterClickHandler = (event) => {
+  event.preventDefault();
+  
+  parks.sort(sortByRating);
+
+  render();
+};
+
+/* function to handle the ratingSorter click - before RENDER REFACTORED
 const ratingSorterClickHandler = (event) => {
   event.preventDefault();
 
@@ -132,7 +156,53 @@ const ratingSorterClickHandler = (event) => {
   parksArray.forEach((park) => {
     main.appendChild(park);
   });
+}; */
+
+///////////* RENDER *//////////
+
+const renderOnePark = (park) => {
+  // Get the individual properties of the park
+  const { name, location, description, established, area, rating } = park;
+
+  const content = `
+      <section class="park-display">
+        <h2>${name}</h2>
+        <div class="location-display">${location}</div>
+        <div class="description-display">${description}</div>
+        <button class="rate-button" title="Add to Favourites">&#9734;</button>
+        <div class="stats">
+          <div class="established-display stat">
+            <h3>Established</h3>
+            <div class="value">${established}</div>
+          </div>
+          <div class="area-display stat">
+            <h3>Area</h3>
+            <div class="value">${area}</div>
+          </div>
+          <div class="rating-display stat">
+            <h3>Rating</h3>
+            <div class="value">${rating}</div>
+          </div>
+        </div>
+      </section>
+  `;
+  return content;
 };
+
+const render = () => {
+  // Get the parent element
+  const main = document.querySelector("main");
+
+  // Empty the parent element
+  main.innerHTML = "";
+
+  // Get the parks HTML
+  const content = parks.map(renderOnePark).join("");
+
+  // Set the `innerHTML` of parent element
+  main.innerHTML = content;
+};
+
 
 // the point where all the code starts
 const main = () => {
@@ -149,18 +219,27 @@ const main = () => {
   ratingSorter.addEventListener("click", ratingSorterClickHandler);
 
   // select all the buttons for all the parks
-  const allBtns = document.querySelectorAll(".rate-button");
+  const main = document.querySelector("main"); // RENDER REFACTORED
+  // const allBtns = document.querySelectorAll(".rate-button"); //BEFORE RENDER REFACTOR
 
-  // iterate the list of buttons and add an event handler to each
+  // Add event handler to the main
+  main.addEventListener("click", favoriteButtonClickHandler);
+    if (event.target && event.target.nodeName == "BUTTON") {
+      const park = event.target.parentNode;
+      park.style.backgroundColor = "#c8e6c9";
+    }
+  /* iterate the list of buttons and add an event handler to each - BEFORE RENDER REFACTOR
   allBtns.forEach((btn) => {
     btn.addEventListener("click", favoriteButtonClickHandler);
-  });
+  }); */
 
   // get the form element
   const form = document.querySelector("#park-form");
 
   // attach the submit handler
   form.addEventListener("submit", submitHandler);
+  
+  render();
 };
 
 // Add event listener for DOMContentLoaded
